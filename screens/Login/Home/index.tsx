de264ef3a321afa3fb73a0a0121f4a61f201e64b
juansie96 <juansie.96@gmail.com>
2023-02-13 13:14:51 -0300

    Create home component

---
+++ screens/Login/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@rneui/themed";
+import { View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
