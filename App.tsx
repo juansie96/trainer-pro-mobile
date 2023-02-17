65aff0a30baaac9cc669eb15c0f44321d5c4b229
juansie96 <juansie.96@gmail.com>
2023-02-16 22:26:48 -0300

    Small refactor on App

--- App.tsx
+++ App.tsx
@@ -1,45 +1,46 @@
 import Login from "./screens/Login";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./firebase/firebase";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
 import LoadingOverlay from "./components/atoms/LoadingOverlay";
 import Logout from "./components/atoms/Logout";
+import { UserContextProvider } from "./hooks/useUser";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase/firebase";
+export { useUser } from "./hooks";
 
 export default function App() {
-  const [user, loading] = useAuthState(auth);
   const Stack = createNativeStackNavigator();
+  const [session, isSessionLoading] = useAuthState(auth);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="main">
-        {loading ? (
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="LoadingOverlay"
-            component={LoadingOverlay}
-          />
-        ) : user ? (
-          <>
+    <UserContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="main">
+          {isSessionLoading ? (
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="LoadingOverlay"
+              component={LoadingOverlay}
+            />
+          ) : session ? (
             <Stack.Screen
               name="Home"
               component={Home}
               options={{
-                headerTitle: "TrainerPro",
+                headerTitle: "TP",
                 headerRight: () => <Logout />,
               }}
             />
-          </>
-        ) : (
-          <>
+          ) : (
             <Stack.Screen
               name="Login"
               component={Login}
               options={{ headerShown: false }}
             />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContextProvider>
   );
 }
