192ef722ca31dd6552af0581f88a48f0b6175f60
juansie96 <juansie.96@gmail.com>
2023-02-16 22:30:02 -0300

    Initial logic to fetch user data on Home screen

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,10 +1,18 @@
 import { Text } from "@rneui/themed";
 import { View } from "react-native";
-
+import { useUser } from "../../hooks";
+import { useEffect } from "react";
 const Home = () => {
+  const { userData, isFetching, error, fetchUserData } = useUser();
+  console.log({ userData, isFetching, error });
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ paddingHorizontal: 15, marginTop: 15 }}>
+      <Text>Buenas tardes, Usuario</Text>
     </View>
   );
 };
